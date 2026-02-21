031747531f5c18ee94a6da34d81f7282d2e3621f
skipajenkins <skipajenkins@users.noreply.github.com>
2026-02-21 13:42:43 +0000

    adding a generic function

--- main.go
+++ main.go
@@ -1,31 +1,34 @@
-
 package main
 
 import "fmt"
 
 func main() {
-	//Intialize a map for the integer values
+	// Intialize a map for the integer values
 	ints := map[string]int64{
-		"first": 34,
-		"second":12,
+		"first":  34,
+		"second": 12,
 	}
 
 	// Initialize a map for the float values
 	floats := map[string]float64{
-		"first": 35.98,
+		"first":  35.98,
 		"second": 26.99,
 	}
 
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
 		SumFloats(floats))
+
+	fmt.Printf("Ge`neric Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
 }
 
-//SumInts adds together the values of m.
+// SumInts adds together the values of m.
 func SumInts(m map[string]int64) int64 {
 	var s int64
 	for _, v := range m {
-		s +=v
+		s += v
 	}
 	return s
 }
@@ -39,3 +42,12 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
+// SumIntsOrFloats sums the values of map m.
+// It supports both int64 and float64 as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
