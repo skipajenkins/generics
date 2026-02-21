d4b9e82ddfad2c76f3d767b15e13174f223c2f55
skipajenkins <skipajenkins@users.noreply.github.com>
2026-02-21 13:49:29 +0000

    removed type argument

--- main.go
+++ main.go
@@ -19,9 +19,13 @@ func main() {
 		SumInts(ints),
 		SumFloats(floats))
 
-	fmt.Printf("Ge`neric Sums: %v and %v\n",
+	fmt.Printf("Generic Sums: %v and %v\n",
 		SumIntsOrFloats[string, int64](ints),
 		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred::w %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
 }
 
 // SumInts adds together the values of m.
